2eb85b5030e94457508e37974e613e22fed2a1cf
Tai Huynh <taihuynh@Tais-MacBook-Air.local>
2019-10-25 12:29:51 +1100

    insert categories to db from admin site

--- admin/categories.php
+++ admin/categories.php
@@ -15,9 +15,25 @@
                         <small>Author</small>
                     </h1>
                     <div class="col-xs-6">
-                        <form action="">
+                        <!-- insert categories to db from admin -->
+                        <?php
+                        if (isset($_POST['submit'])) {
+                            $cat_title = $_POST['cat_title'];
+                            if ($cat_title == "" || empty($cat_title)) {
+                                echo "This Field should not be empty";
+                            } else {
+                                $query = "INSERT INTO categories(cat_title)";
+                                $query .= "VALUE('{$cat_title}')";
+                                $result = mysqli_query($connection, $query);
+                                if (!$result) {
+                                    die("Query Failed" . mysqli_error($connection));
+                                }
+                            }
+                        }
+                        ?>
+                        <form action="" method="post">
                             <div class="form-group">
-                                <label for="cat-title">Add Category</label>
+                                <label for="cat_title">Add Category</label>
                                 <input type="text" class="form-control" name="cat_title">
                             </div>
                             <div class="form-group">
